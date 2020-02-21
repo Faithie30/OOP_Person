74e8a9ffdc62d1df661dbdb22142fcdff4517289
Faithie30 <56162583+Faithie30@users.noreply.github.com>
2020-02-21 12:06:03 +0200

    Add files via upload

---
+++ oop.py
@@ -0,0 +1,16 @@
+class Person:
+    def __init__(self, name, age, gender, interests):
+        self.name       = name
+        self.age        = age
+        self.gender     = gender
+        self.interests  = interests
+
+    def hello(self):
+        list        = ' , '.join(self.interests)
+        print 'Hello, my name is' , self.name, 'and i am',  self.age, 'years old. My interests are',  list
+
+
+
+person1 = Person("Ryan" , 30 ,"male",["being a hardarse", "agile," + " and " "ssd hard drive."])
+greeting = person1.hello()
+print(greeting)
