0904eb8403f02d1f352abc878cf3548cf06f06b3
Faithie30 <56162583+Faithie30@users.noreply.github.com>
2020-02-23 22:59:55 +0200

    updating

--- oop.py
+++ oop.py
@@ -1,17 +1,18 @@
 #Creating a class named Person
 class Person:
+
     #Assigning values to object properties 
     def __init__(self, name, age, gender, interests):
         self.name       = name
         self.age        = age
         self.gender     = gender
         self.interests  = ' , '.join(interests)
+
     #Creating an object hello
     def hello(self):
-      # print 'Hello, my name is' , self.name, 'and i am',  self.age, 'years old. My interests are',  self.interests
-        # return f'Hello, my name is {self.name}, and i am {self.age} years old. My interests are:' self.interests
         return 'Hello, my name is {} and I am {} years old. My interests are {}'.format(self.name,self.age,self.interests)
 
+#Calling the class Person
 person1 = Person("Ryan" , 30 ,"male",["being a hardarse", "agile," , "ssd hard drive."])
 greeting = person1.hello()
 print(greeting)
